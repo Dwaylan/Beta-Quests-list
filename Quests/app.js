bc6c3704fce185fd41cad53145df59c395ea3c02
Dwaylan <Dwaylan.Applewhite@gmail.com>
2024-07-12 15:52:09 -0400

    Chore: added clock logic underneath the list

--- Quests/app.js
+++ Quests/app.js
@@ -88,3 +88,38 @@ search.addEventListener('keyup', e =>{
     // passing search values up to the filter function
     filterTodos(searchValue)
 })
+
+// New additions for the clock element
+
+// Grabbing the clock class via query selector
+const clock = document.querySelector('.clock')
+
+const tick = () =>{
+    // Creating a new date variable for every new second
+    const now = new Date();
+    
+    // Creating a variable to convert milliseconds to hours
+    const hours = now.getHours()
+
+    // Creating a variable to convert milliseconds to minutes
+    const minutes = now.getMinutes()
+
+    // Creating a variable to convert milliseconds to seconds
+    const seconds = now.getSeconds()
+
+    // Additional formatting for year
+    const year = dateFns.format(now, 'YYYY')
+
+    // Creating an HTML template with template literals to append the time to the DOM
+    const html = `
+        <span>${hours}</span> :
+        <span>${minutes}</span> :
+        <span>${seconds}</span>
+
+        <span>${year}</span>
+    `
+    // injecting the new html template to the DOM html
+    clock.innerHTML = html
+}
+// Using the setInterval method to tick for every 1000 milliseconds or 1 second
+setInterval(tick, 1000)
